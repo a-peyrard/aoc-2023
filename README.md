3a75bdb6116698f50ee54fd2f56555efc0336acd
Augustin Peyrard <augustin.peyrard@gmail.com>
2023-12-07 20:56:41 -0800

    part2 math way
    from 15ms to 260ns :D
    Nothing fancy, just some basic high school math.

--- README.md
+++ README.md
@@ -25,14 +25,14 @@ _Template for the repository: http://github.com/fspoettel/advent-of-code-rust_
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `57.7µs` | `747.3µs` |
-| [Day 2](./src/bin/02.rs) | `40.6µs` | `39.8µs` |
-| [Day 3](./src/bin/03.rs) | `70.8µs` | `95.7µs` |
-| [Day 4](./src/bin/04.rs) | `249.0µs` | `253.5µs` |
-| [Day 5](./src/bin/05.rs) | `57.0µs` | `98.1µs` |
-| [Day 6](./src/bin/06.rs) | `281.0ns` | `15.2ms` |
+| [Day 1](./src/bin/01.rs) | `59.0µs` | `752.1µs` |
+| [Day 2](./src/bin/02.rs) | `42.1µs` | `40.1µs` |
+| [Day 3](./src/bin/03.rs) | `73.5µs` | `99.8µs` |
+| [Day 4](./src/bin/04.rs) | `254.0µs` | `258.8µs` |
+| [Day 5](./src/bin/05.rs) | `57.6µs` | `98.7µs` |
+| [Day 6](./src/bin/06.rs) | `196.0ns` | `260.0ns` |
 
-**Total: 16.91ms**
+**Total: 1.74ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -57,25 +57,22 @@ impl Race {
     }
 
     fn winning_possibilities(&self) -> u64 {
-        let mut winning_possibilities = 0;
-        let pivot = self.time / 2;
-        for n in pivot..self.time + 1 {
-            if Race::distance_at(self.time, n) > self.distance {
-                winning_possibilities += 1;
-            }
+        let t = self.time as f64;
+        let d = self.distance as f64;
+
+        let low: f64 = (t - ((t * t) - 4f64 * d).sqrt()) / 2f64;
+        let high: f64 = (t + ((t * t) - 4f64 * d).sqrt()) / 2f64;
+
+        let mut low_int = low.ceil() as i64;
+        if low.fract() == 0f64 {
+            low_int += 1;
+        }
+        let mut high_int = high.floor() as i64;
+        if high.fract() == 0f64 {
+            high_int -= 1;
         }
 
-        for n in (0..pivot).rev() {
-            if Race::distance_at(self.time, n) > self.distance {
-                winning_possibilities += 1;
-            }
-        }
-
-        winning_possibilities
-    }
-
-    fn distance_at(time: u64, press: u64) -> u64 {
-        (time - press) * press
+        (high_int - low_int + 1) as u64
     }
 }
 
@@ -108,22 +105,26 @@ mod tests {
     }
 
     #[test]
-    fn test_should_compute_distance_at_examples() {
-        // Don't hold the button at all (that is, hold it for 0 milliseconds) at the start of the race. The boat won't move; it will have traveled 0 millimeters by the end of the race.
-        // Hold the button for 1 millisecond at the start of the race. Then, the boat will travel at a speed of 1 millimeter per millisecond for 6 milliseconds, reaching a total distance traveled of 6 millimeters.
-        // Hold the button for 2 milliseconds, giving the boat a speed of 2 millimeters per millisecond. It will then get 5 milliseconds to move, reaching a total distance of 10 millimeters.
-        // Hold the button for 3 milliseconds. After its remaining 4 milliseconds of travel time, the boat will have gone 12 millimeters.
-        // Hold the button for 4 milliseconds. After its remaining 3 milliseconds of travel time, the boat will have gone 12 millimeters.
-        // Hold the button for 5 milliseconds, causing the boat to travel a total of 10 millimeters.
-        // Hold the button for 6 milliseconds, causing the boat to travel a total of 6 millimeters.
-        // Hold the button for 7 milliseconds. That's the entire duration of the race. You never let go of the button. The boat can't move until you let go of the button. Please make sure you let go of the button so the boat gets to move. 0 millimeters.
-        assert_eq!(Race::distance_at(7, 0), 0);
-        assert_eq!(Race::distance_at(7, 1), 6);
-        assert_eq!(Race::distance_at(7, 2), 10);
-        assert_eq!(Race::distance_at(7, 3), 12);
-        assert_eq!(Race::distance_at(7, 4), 12);
-        assert_eq!(Race::distance_at(7, 5), 10);
-        assert_eq!(Race::distance_at(7, 6), 6);
-        assert_eq!(Race::distance_at(7, 7), 0);
+    fn test_winning_possibilities_1() {
+        assert_eq!(
+            Race {
+                time: 7,
+                distance: 9
+            }
+            .winning_possibilities(),
+            4
+        );
+    }
+
+    #[test]
+    fn test_winning_possibilities_2() {
+        assert_eq!(
+            Race {
+                time: 15,
+                distance: 40
+            }
+            .winning_possibilities(),
+            8
+        );
     }
 }
